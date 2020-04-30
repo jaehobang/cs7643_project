1b0fe73c12ba081c7f8275c90f15fabd96621444
Jaeho Bang <jaehob@andrew.cmu.edu>
2020-04-30 16:53:24 -0400

    Adding training script for DEC

--- others/ptdec_eva/examples/uadetrac.py
+++ others/ptdec_eva/examples/uadetrac.py
@@ -7,6 +7,8 @@ from others.ptdec_eva.ptdec.dec import DEC
 from others.ptdec_eva.ptdec.model import train, predict
 from others.ptdec_eva.ptsdae.ptsdae.sdae import StackedDenoisingAutoEncoder
 
+
+import os
 import others.ptdec_eva.ptsdae.ptsdae.model as ae
 import numpy as np
 import torch
@@ -76,124 +78,6 @@ class CachedUAD(Dataset):
 
 
 
-def main(
-    cuda,
-    batch_size,
-    pretrain_epochs,
-    finetune_epochs,
-    testing_mode
-):
-    # callback function to call during training, uses writer from the scope
-
-    def training_callback(epoch, lr, loss, validation_loss):
-        pass
-
-
-    CLUSTER_NUM = -1 ## TODO: we need to set this number depending on size of test dataset
-
-
-    loader = UADetracLoader()
-    images = loader.load_cached_images(name = 'uad_test_images.npy', vi_name = 'uad_test_vi.npy')
-    labels = loader.load_cached_labels(name = 'uad_test_labels.npy')
-    labels = labels['vehicle']
-    boxes = loader.load_cached_boxes(name = 'uad_test_boxes.npy')
-    images, labels, boxes = loader.filter_input3(images, labels, boxes)
-
-    ## we need to skip some frames, and use some frames that have been skipped for validation
-    skip_rate = 15
-    val_skip_rate = skip_rate * 3 + 1
-    images_train = images[::skip_rate]
-    labels_train = labels[::skip_rate]
-    boxes_train = boxes[::skip_rate]
-
-    images_test = images[::val_skip_rate] ## just a random number that makes the validation set small enough
-    labels_test = labels[::val_skip_rate]
-    boxes_test = boxes[::val_skip_rate]
-
-    ## we need to resize the images.... try 28 x 28
-    images_train = images_train[:,::11,::11,:]
-    images_train = np.mean(images_train, axis = 3)
-    images_train = images_train.astype(np.uint8)
-    print(images_train.shape)
-    assert(images_train.shape[1] == 28)
-    assert(images_train.shape[2] == 28)
-
-    images_test = images_test[:,::11,::11,:]
-    images_test = np.mean(images_test, axis = 3)
-    images_test = images_test.astype(np.uint8)
-    assert(images_test.shape[1] == 28)
-    assert(images_test.shape[2] == 28)
-
-    train_dataset = CachedUAD()
-    train_dataset.set_images(images_train)
-    train_dataset.set_labels(labels_train)
-
-    test_dataset = CachedUAD()
-    test_dataset.set_images(images_test)
-    test_dataset.set_labels(labels_test)
-
-
-    autoencoder = StackedDenoisingAutoEncoder(
-        [28 * 28, 500, 500, 2000, 10],
-        final_activation=None
-    )
-    if cuda:
-        autoencoder.cuda()
-    print('Pretraining stage.')
-    ae.pretrain(
-        train_dataset,
-        autoencoder,
-        cuda=cuda,
-        validation=test_dataset,
-        epochs=pretrain_epochs,
-        batch_size=batch_size,
-        optimizer=lambda model: SGD(model.parameters(), lr=0.1, momentum=0.9),
-        scheduler=lambda x: StepLR(x, 100, gamma=0.1),
-        corruption=0.2, epoch_callback = None
-    )
-    print('Training stage.')
-    ae_optimizer = SGD(params=autoencoder.parameters(), lr=0.1, momentum=0.9)
-    ae.train(
-        train_dataset,
-        autoencoder,
-        cuda=cuda,
-        validation=test_dataset,
-        epochs=finetune_epochs,
-        batch_size=batch_size,
-        optimizer=ae_optimizer,
-        scheduler=StepLR(ae_optimizer, 100, gamma=0.1),
-        corruption=0.2,
-        update_callback=training_callback
-    )
-    print('DEC stage.')
-    model = DEC(
-        cluster_number=CLUSTER_NUM,
-        hidden_dimension=10,
-        encoder=autoencoder.encoder
-    )
-    if cuda:
-        model.cuda()
-    dec_optimizer = SGD(model.parameters(), lr=0.01, momentum=0.9)
-    train(
-        dataset=train_dataset,
-        model=model,
-        epochs=100,
-        batch_size=256,
-        optimizer=dec_optimizer,
-        stopping_delta=0.000001,
-        cuda=cuda
-    )
-
-    save_model('autoencoder', autoencoder)
-    save_model('dec', model)
-
-
-    predicted = predict(test_dataset, model, 1024, silent=True, return_actual=False, cuda=cuda)
-    #actual = actual.cpu().numpy()
-    #predicted = predicted.cpu().numpy()
-    #reassignment, accuracy = cluster_accuracy(actual, predicted)
-    #print('Final DEC accuracy: %s' % accuracy)
-
 
 
 def save_model(model_name, model):
@@ -242,15 +126,116 @@ if __name__ == '__main__':
     batch_size = 256
     cuda = True
     pretrain_epochs = 300
-    finetune_epochs = 300
+    finetune_epochs = 10000
     testing_mode = False
-    main(cuda, batch_size, pretrain_epochs, finetune_epochs, testing_mode)
 
-    """
-    Now, it runs, so we need to start making the APIs for experiments
-    
-    ###TODOS:
-    1. We need to save the model parameters
-    2. We need to make an API for evaluation of DEC -- clustering the frames for sampling
-    3. We need to evaluate using SSD
-    """
+    # callback function to call during training, uses writer from the scope
+
+    def training_callback(epoch, lr, loss, validation_loss):
+        pass
+
+
+    CLUSTER_NUM = 600 ## TODO: we need to set this number depending on size of test dataset
+
+    loader = UADetracLoader()
+    images = loader.load_images(dir='/nethome/jbang36/eva_jaeho/data/ua_detrac/test_images')
+    labels, boxes = loader.load_labels(dir='/nethome/jbang36/eva_jaeho/data/ua_detrac/test_xml')
+    labels = labels['vehicle']
+
+    images, labels, boxes = loader.filter_input3(images, labels, boxes)
+
+    ## we need to skip some frames, and use some frames that have been skipped for validation
+    skip_rate = 15
+    val_skip_rate = skip_rate * 3 + 1
+    images_train = images[::skip_rate]
+    labels_train = labels[::skip_rate]
+    boxes_train = boxes[::skip_rate]
+
+    images_test = images[::val_skip_rate]  ## just a random number that makes the validation set small enough
+    labels_test = labels[::val_skip_rate]
+    boxes_test = boxes[::val_skip_rate]
+
+    ## we need to resize the images.... try 28 x 28
+    images_train = images_train[:, ::11, ::11, :]
+    images_train = np.mean(images_train, axis=3)
+    images_train = images_train.astype(np.uint8)
+    print(images_train.shape)
+    assert (images_train.shape[1] == 28)
+    assert (images_train.shape[2] == 28)
+
+    images_test = images_test[:, ::11, ::11, :]
+    images_test = np.mean(images_test, axis=3)
+    images_test = images_test.astype(np.uint8)
+    assert (images_test.shape[1] == 28)
+    assert (images_test.shape[2] == 28)
+
+    train_dataset = CachedUAD()
+    train_dataset.set_images(images_train)
+    train_dataset.set_labels(labels_train)
+
+    test_dataset = CachedUAD()
+    test_dataset.set_images(images_test)
+    test_dataset.set_labels(labels_test)
+
+    autoencoder = StackedDenoisingAutoEncoder(
+        [28 * 28, 500, 500, 2000, 10],
+        final_activation=None
+    )
+    if cuda:
+        autoencoder.cuda()
+
+    print('Pretraining stage.')
+    ae.pretrain(
+        train_dataset,
+        autoencoder,
+        cuda=cuda,
+        validation=test_dataset,
+        epochs=pretrain_epochs,
+        batch_size=batch_size,
+        optimizer=lambda model: SGD(model.parameters(), lr=0.1, momentum=0.9),
+        scheduler=lambda x: StepLR(x, 100, gamma=0.1),
+        corruption=0.2, epoch_callback=None
+    )
+
+    print('Training stage.')
+    ae_optimizer = SGD(params=autoencoder.parameters(), lr=0.1, momentum=0.9)
+    ae.train(
+        train_dataset,
+        autoencoder,
+        cuda=cuda,
+        validation=test_dataset,
+        epochs=finetune_epochs,
+        batch_size=batch_size,
+        optimizer=ae_optimizer,
+        scheduler=StepLR(ae_optimizer, 100, gamma=0.1),
+        corruption=0.2,
+        update_callback=training_callback
+    )
+
+    print('DEC stage.')
+    model = DEC(
+        cluster_number=CLUSTER_NUM,
+        hidden_dimension=10,
+        encoder=autoencoder.encoder
+    )
+    if cuda:
+        model.cuda()
+    dec_optimizer = SGD(model.parameters(), lr=0.01, momentum=0.9)
+    train(
+        dataset=train_dataset,
+        model=model,
+        epochs=1000,
+        batch_size=256,
+        optimizer=dec_optimizer,
+        stopping_delta=0.000001,
+        cuda=cuda
+    )
+
+
+    ## we save the model and use load_model to load it
+    save_model('autoencoder', autoencoder)
+    save_model('dec', model)
+
+    predicted = predict(test_dataset, model, 1024, silent=True, return_actual=False, cuda=cuda)
+
+
